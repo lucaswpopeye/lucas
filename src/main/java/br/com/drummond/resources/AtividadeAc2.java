526a7319456fda6ff3df57d3cf9834a1f4bb0d89
lucaswpopeye <lucaswpopeye@gmail.com>
2019-10-16 22:33:41 -0300

    porra

---
+++ src/main/java/br/com/drummond/resources/AtividadeAc2.java
@@ -0,0 +1,27 @@
+package br.com.drummond.resources;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AtividadeAc2 {
+	
+	
+	@RequestMapping(value = "/multiplo8/{valor1}", method = RequestMethod.GET)
+	public String resposta(@PathVariable Integer valor1) {
+		String sim = "sim";
+		String nao = "nao";
+		Integer resultado; 
+	
+		Integer v1 = 8;
+		
+		resultado = valor1 % v1;
+		if (resultado == 0)
+
+		return sim;
+		else 
+			return nao;
+	}
+}
